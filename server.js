e13b845bc0a28b0b2b4d4af9b4fe6b4b5b1b9058
mikhail-cct <tayemnyj+cct@gmail.com>
2020-03-11 22:40:26 +0000

    wk4 complete

--- server.js
+++ server.js
@@ -7,16 +7,24 @@ mongoose = require('mongoose');
 
 var app = express();
 var port = 3000;
+var userCtrl = require('./user-controller');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(require('./routes'));
+
+app.post('/users', userCtrl.createUser);
+app.get('/users', userCtrl.getUsers);
+app.get('/users/:id', userCtrl.getUser);
+app.delete('/users/:id', userCtrl.deleteUser);
+app.put('/users/:id', userCtrl.updateUser);
 
 app.listen(port, function(err){
     console.log("Listening on Port: " + port)
 });
 
 mongoose.connect('mongodb://localhost/test');
+
 mongoose.connection.on('error', (err) => { 
     console.log('Mongodb Error: ', err); 
     process.exit();
