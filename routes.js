1fb010f84645fecdc06e5c4a5984aa9d9543b386
mikhail-cct <tayemnyj+cct@gmail.com>
2020-04-09 01:10:35 +0000

    wk8

--- routes.js
+++ routes.js
@@ -1,11 +1,22 @@
 var express = require('express'),
 router = express.Router(),
-itemCtrl = require('./user-controller');
+userCtrl = require('./user-controller');
+
+module.exports.UPLOAD_PATH = 'uploads';
+
+var multer = require('multer'),
+upload = multer( { dest: module.exports.UPLOAD_PATH }),
+imageCtrl = require('./image-controller');
 
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
 router.get('/users/:id', userCtrl.getUser);
 router.delete('/users/:id', userCtrl.deleteUser);
 router.put('/users/:id', userCtrl.updateUser);
+
+router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+router.get('/images/:id', imageCtrl.getImage);
+router.delete('/images/:id', imageCtrl.deleteImage);
 
 module.exports = router;
